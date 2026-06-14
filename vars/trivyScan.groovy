94ada5b6cb6f0818fb4fb3400e1ca9d6635a0063
Ayoub Khadrani <115008402+KhadraniAyoub@users.noreply.github.com>
2026-06-14 02:39:22 +0100

    Commented trivy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,6 +1,6 @@
 def call(String imageName, String tag) {
     echo "Scanning image for vulnerabilities: ${imageName}:${tag}"
-    sh "trivy image --exit-code 0 --severity HIGH,CRITICAL ${imageName}:${tag}"
+    //sh "trivy image --exit-code 0 --severity HIGH,CRITICAL ${imageName}:${tag}"
     // exit-code 0 for local testing (don't fail on vulns during demo)
     // in production: --exit-code 1
 }
