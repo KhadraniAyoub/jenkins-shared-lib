c772511cee8b36e4e4be81b598efd2d6d82779bb
Ayoub Khadrani <115008402+KhadraniAyoub@users.noreply.github.com>
2026-06-14 14:26:10 +0100

    Added Trivy back

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -1,6 +1,10 @@
 def call(String imageName, String tag) {
-    echo "Scanning image for vulnerabilities: ${imageName}:${tag}"
-    //sh "trivy image --exit-code 0 --severity HIGH,CRITICAL ${imageName}:${tag}"
-    // exit-code 0 for local testing (don't fail on vulns during demo)
-    // in production: --exit-code 1
+    echo "Scanning ${imageName}:${tag} for vulnerabilities"
+    sh """
+        trivy image \
+          --exit-code 0 \
+          --severity HIGH,CRITICAL \
+          --format table \
+          ${imageName}:${tag}
+    """
 }
