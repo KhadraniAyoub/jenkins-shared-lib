bc6f62e39a7a8467108e66274b8f1e7163d523cd
Ayoub Khadrani <115008402+KhadraniAyoub@users.noreply.github.com>
2026-06-14 21:29:41 +0100

    Added harbor

--- vars/pushToRegistry.groovy
+++ vars/pushToRegistry.groovy
@@ -1,5 +1,20 @@
-def call(String imageName, String tag) {
-    echo "Pushing image to registry: ${imageName}:${tag}"
-    // In real setup: withCredentials([...]) { sh "docker push ..." }
-    echo "Push simulated for local environment"
+def call(String imageName, String tag, String harborUrl, String project) {
+    echo "Pushing ${imageName}:${tag} to Harbor"
+    
+    withCredentials([usernamePassword(
+        credentialsId: 'harbor-credentials',
+        usernameVariable: 'HARBOR_USER',
+        passwordVariable: 'HARBOR_PASS'
+    )]) {
+        sh """
+            docker login ${harborUrl} \
+              -u ${HARBOR_USER} \
+              -p ${HARBOR_PASS}
+            
+            docker tag ${imageName}:${tag} \
+              ${harborUrl}/${project}/${imageName}:${tag}
+            
+            docker push ${harborUrl}/${project}/${imageName}:${tag}
+        """
+    }
 }
